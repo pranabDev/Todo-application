d552d9fb6299d90b88f4ca914949632172d11fce
pranab56 <pronabhalder53@gmail.com>
2023-07-18 17:20:19 +0600

    design done

--- src/components/Todo_design.jsx
+++ src/components/Todo_design.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import image from "../image/agenda.png";
 import image2 from "../image/checklist.png";
-import editing from '../image/editing.png';
-import deleteimage from '../image/delete.png';
+import editing from "../image/editing.png";
+import deleteimage from "../image/delete.png";
+
 
 const Todo_design = () => {
   return (
-    <div class="max-w-xl mx-auto h-screen px-8 py-10 bg-white rounded-lg shadow-md dark:bg-gray-800">
+    <div class="max-w-xl mx-auto h-screen overflow-scroll px-8 py-10 bg-white rounded-lg shadow-md dark:bg-gray-800">
       {/* title */}
       <div className="flex justify-center align-middle items-center">
         <img className="w-7 h-7 mt-1" src={image} alt="" />
@@ -24,7 +25,7 @@ const Todo_design = () => {
             type="text"
             placeholder="Add your todo"
           />
-          <span class="flex items-center bg-gray-100 rounded rounded-l-none border-0 px-3 font-bold text-grey-100">
+          <span class="flex items-center bg-gray-100 rounded rounded-l-none px-3 font-bold text-grey-100">
             <button class="px-6 py-2 btn font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-green-600 rounded-lg hover:bg-green-600 focus:outline-none focus:ring focus:ring-green-600 focus:ring-opacity-80">
               Add
             </button>
@@ -39,14 +40,33 @@ const Todo_design = () => {
 
       {/* todo list */}
       <div className="flex justify-between items-center mt-10">
-       <span className="flex items-center">
-       <input className="checkbox checkbox-info" type="checkbox" name="" id="todo"/>
-        <label className="ml-2" for="todo"> I have a bike</label>
-       </span>
-       <span className="flex">
-        <img className="w-5 h-5 cursor-pointer" src={editing} alt="" />
-        <img className="w-5 h-5 ml-5 cursor-pointer" src={deleteimage} alt="" />
-       </span>
+        <span className="flex items-center">
+          <input
+            className="checkbox checkbox-info"
+            type="checkbox"
+            name=""
+            id="todo"
+          />
+          <label className="ml-2" for="todo">
+            {" "}
+            I have a bike
+          </label>
+        </span>
+        <span className="flex">
+          <img className="w-5 h-5 cursor-pointer" src={editing} alt="" />
+          <img
+            className="w-5 h-5 ml-5 cursor-pointer"
+            src={deleteimage}
+            alt=""
+          />
+        </span>
+      </div>
+
+      {/* last  */}
+      <div className="flex items-center  justify-between mt-5">
+        <button className="btn btn-info">Filter</button>
+        <h3>Completed:1</h3>
+        <h3 className="text-gray-500">Total Task:2</h3>
       </div>
     </div>
   );
---
+++ src/components/Todos.jsx
@@ -0,0 +1,8 @@
+const Todos=[
+    {todo:'i am playing football'},
+    {todo:'i am watching movie'},
+    {todo:'i am sleaping new'},
+    {todo:'i am reading now'},
+];
+
+export default Todos;
