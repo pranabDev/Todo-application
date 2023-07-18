9b2185395ff71e8943547c3c1a02186016270f45
pranab56 <pronabhalder53@gmail.com>
2023-07-18 14:54:11 +0600

    image size done

--- src/components/Todo_design.jsx
+++ src/components/Todo_design.jsx
@@ -6,7 +6,7 @@ const Todo_design = () => {
   return (
     <div class="max-w-2xl mx-auto h-screen px-8 py-10 bg-white rounded-lg shadow-md dark:bg-gray-800">
       <div className="flex justify-center align-middle items-center">
-        <img className="w-7 h-7 mt-1" src={image} alt="" />
+        <img className="w-8 h-7 mt-1" src={image} alt="" />
         <h3 className="ml-2 text-3xl font-bold text-green-600">Todo List</h3>
       </div>
 
